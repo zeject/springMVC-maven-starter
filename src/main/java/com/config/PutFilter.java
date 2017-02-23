b8226c9fba9b2fca8efb9a4a33160bf4f8308276
zeject <424049902@qq.com>
2017-02-23 11:07:39 +0800

    change to spring 4.3

--- src/main/java/com/config/PutFilter.java
+++ src/main/java/com/config/PutFilter.java
@@ -3,6 +3,6 @@ package com.config;
 import javax.servlet.annotation.WebFilter;
 
 @WebFilter(filterName = "httpPutFormcontentFilter", description = "过滤器", urlPatterns = "/*")
-public class PutFilter extends org.springframework.web.filter.HttpPutFormContentFilter {
+public class PutFilter extends org.springframework.web.filter.HiddenHttpMethodFilter {
 
 }
---
+++ src/main/java/com/test/Spring4_3.java
@@ -0,0 +1,14 @@
+package com.test;
+
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Spring4_3 {
+
+	@PutMapping(value = "/putUrl")
+	public String put() {
+		return "put";
+	}
+
+}
